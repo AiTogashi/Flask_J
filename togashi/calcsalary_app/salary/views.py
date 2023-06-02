fac9fc5b6e3f53a7de4c6bcb9ddeb20b84d91051
AiTogashi <togashi.ai.ve@mynavi.jp>
2023-06-02 05:13:17 +0000

    salary計算追加

--- togashi/calcsalary_app/salary/views.py
+++ togashi/calcsalary_app/salary/views.py
@@ -1,5 +1,37 @@
+from flask import request, redirect, url_for, render_template, flash, session
 from salary import app
+from decimal import Decimal, ROUND_HALF_UP
 
-@app.route('/')
+@app.route('/',methods=['GET','POST'])
 def show_entries():
-    return "Hello World"
+    # return render_template('input.html')
+    input_data = session.get('input_data', None)
+    return render_template("input.html", input = input_data)    
+
+@app.route('/output',methods=['GET','POST'])
+def calc():
+    session["input_data"]=""
+    if request.form["salary"] == "":
+        flash('給与が未入力です。入力してください。')
+        return redirect(url_for("show_entries"))
+
+    if int(request.form["salary"]) > 9999999999:
+        flash('給与には最大9,999,999,999まで入力可能です。')
+        session["input_data"] = request.form["salary"]
+        return redirect(url_for("show_entries"))
+
+    if int(request.form["salary"]) < 0:
+        flash('給与にはマイナスの値は入力できません。')
+        session["input_data"] = request.form["salary"]
+        return redirect(url_for("show_entries")) 
+    
+    if request.method == "POST":
+        input_salary = int(request.form["salary"])
+        if (input_salary > 1000000):
+            tax = (input_salary - 1000000) * 0.2 + 100000
+        else:
+            tax = input_salary * 0.1
+        tax = Decimal(str(tax)).quantize(Decimal("0"), rounding=ROUND_HALF_UP)
+        #支給額 = 給与 - 税額
+        pay = input_salary - tax
+    return render_template("output.html",  salary=input_salary, tax=tax, pay=pay)
