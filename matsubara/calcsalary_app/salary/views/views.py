a04bfb5b12d5c5d938fcfb391391b5daf3bdae2f
MatsubaraMasaru <matsubara.masaru.nv@mynavi.jp>
2023-06-02 03:51:47 +0000

    salaryの完成

--- matsubara/calcsalary_app/salary/views/views.py
+++
@@ -1,8 +0,0 @@
-from flask import request,redirect,url_for,render_template,flash,session
-from salary import app
-
-@app.route('/')
-def show_entries():
-    if not session.get('logged_in'):
-        return redirect('/login')
-    return render_template('entries/input.html')
