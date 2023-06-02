21e235e184256331548b28bd0791b3fb02f526d6
KoukiTakayamamy <takayama.kouki.va@mynavi.jp>
2023-06-02 06:02:33 +0000

    add

--- takayama/calcsalary_app/salary/__init__.py
+++ takayama/calcsalary_app/salary/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask
 
 app = Flask(__name__)
-app.config.from_object("flask_blog.config")
+app.config.from_object("salary.config")
 
-import Flask_J.takayama.calcsalary_app.salary.views.views
+import salary.views
 
