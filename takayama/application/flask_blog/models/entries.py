d035aef4bdce20e018f49e9e4de094e07da2e79f
KoukiTakayamamy <takayama.kouki.va@mynavi.jp>
2023-06-07 02:27:00 +0000

    add 参考書最終

--- takayama/application/flask_blog/models/entries.py
+++ takayama/application/flask_blog/models/entries.py
@@ -5,7 +5,7 @@ class Entry(db.Model):
     __tablename__ = "entries"
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(50), unique=True)
-    text = db.Column(db.DateTime)
+    text = db.Column(db.Text)
     created_at = db.Column(db.DateTime)
 
     def __init__(self, title=None, text=None):
